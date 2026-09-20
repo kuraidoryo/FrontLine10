906c5ec1b9f788e56ec7df684412abedd2498ce3
kuraidoryo <szymonigambol1@gmail.com>
2026-09-20 14:26:23 +0200

    Add main screen with menu and game logo

---
+++ menu.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const mainMenu = document.getElementById('main-menu');
+    const localMenu = document.getElementById('local-menu');
+
+    document.getElementById('btn-local').addEventListener('click', () => {
+        mainMenu.classList.add('hidden');
+        localMenu.classList.remove('hidden');
+    });
+
+    document.getElementById('btn-back').addEventListener('click', () => {
+        localMenu.classList.add('hidden');
+        mainMenu.classList.remove('hidden');
+    });
+
+    document.getElementById('btn-1v1').addEventListener('click', () => {
+        window.location.href = 'localGame/index.html';
+    });
+});
